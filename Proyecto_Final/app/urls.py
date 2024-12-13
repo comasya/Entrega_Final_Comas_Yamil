e7af4092097ee75b844dce44839fa4646a59756d
Yamil Comas <comasya@gmail.com>
2024-12-12 22:52:27 -0300

    herencia html

--- Proyecto_Final/app/urls.py
+++ Proyecto_Final/app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('inicio/', views.inicio),
+    path('', views.inicio),
     path('contact/', views.contact),
     path('car/', views.car),
     path('blog/', views.blog),
