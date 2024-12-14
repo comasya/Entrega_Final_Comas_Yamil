8bb00e28bc2e073fc5a1e7545ff293ff79892765
Yamil Comas <comasya@gmail.com>
2024-12-14 08:22:29 -0300

    cambio version de django a 4.2

--- Proyecto_Final/app/models.py
+++ Proyecto_Final/app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-
-class about(models.Model):
+class Autos(models.Model):  
     marca = models.CharField(max_length=25)
     modelo = models.CharField(max_length=25)
+    interno = models.IntegerField()
     precio = models.FloatField()
     
 class Cliente(models.Model):
@@ -13,8 +13,8 @@ class Cliente(models.Model):
     email = models.EmailField(max_length=40)
     telefono = models.IntegerField()
     
-class Venta(models.Model):
-    celular = models.CharField(max_length=25)
-    cliente = models.CharField(max_length=40)
+class Alquiler(models.Model):
+    interno = models.IntegerField() 
+    cliente = models.CharField(max_length=50)  
     fecha = models.DateField()
     total = models.FloatField()
