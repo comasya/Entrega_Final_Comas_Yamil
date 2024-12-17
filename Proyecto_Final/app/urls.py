a3bfaaf8a29e6bc1d9018bd677ff731ba013ca14
Yamil Comas <comasya@gmail.com>
2024-12-17 00:18:09 -0300

    modificacion en la navegacion

--- Proyecto_Final/app/urls.py
+++ Proyecto_Final/app/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
-from app import views
+from . import views
 
 urlpatterns = [
-    path('', views.inicio),
-    path('contact/', views.contact),
-    path('car/', views.car),
-    path('blog/', views.blog),
-    path('about/', views.about)    
+    path('', views.inicio, name="Inicio"),
+    path('contacto/', views.contact, name="Contacto"),
+    path('vehiculos/', views.car, name="Vehiculos"),
+    path('blog/', views.blog, name="Blog"),
+    path('nosotros/', views.about, name="Nosotros")    
 ]
